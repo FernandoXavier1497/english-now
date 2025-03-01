de9b1890859676df0395fa775c50f388da2a5305
FernandoXavier <fernandoxavier1497@gmail.com>
2025-02-28 23:54:46 -0300

    autenticação com cookies e primeiro login

---
+++ src/EnglishNow.Web/Models/Usuario/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnglishNow.Web.Models.Usuario
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "O campo Usuário é obrigatorio")]
+        public string? Usuario { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é obrigatorio")]
+        public string? Senha { get; set; }
+
+        public bool LembrarMe { get; set; }
+    }
+}
